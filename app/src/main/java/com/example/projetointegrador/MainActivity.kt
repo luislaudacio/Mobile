a91de727d047e2d55c1051fee5e61312808307dc
LuisaoxD <105399037+LuisaoxD@users.noreply.github.com>
2023-11-03 00:27:02 -0300

    Mudanças Adapter Minha Conta

--- app/src/main/java/com/example/projetointegrador/MainActivity.kt
+++ app/src/main/java/com/example/projetointegrador/MainActivity.kt
@@ -43,54 +43,59 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
     private fun logar () {
-        if(txtEmail.text.toString().isNullOrEmpty() || txtSenha.text.toString().isNullOrEmpty()) {
-            Toast.makeText(this@MainActivity,
-                "Dados Não informados.", Toast.LENGTH_LONG).show()
+        var intent: Intent = Intent(this, MinhaConta:: class.java)
+        startActivity(intent)
 
-            return;
-        }
 
-        usuario = Usuario(
-            access_token = "",
-            email = txtEmail.text.toString(),
-            senha = txtSenha.text.toString()
-        )
 
-        val retrofitCli: RetrofitClient = RetrofitClient()
-        retrofitCli.servicoUsuario.getToken(usuario)
-            .enqueue(object : Callback<Usuario> {
-
-                override fun onResponse(call: Call<Usuario>, response: Response<Usuario>) {
-                    if (!response.isSuccessful) {
-                        Log.e("Usuario ou senha Incorretos", "response: " + response)
-                        Toast.makeText(this@MainActivity, "Usuario ou senha Incorretos", Toast.LENGTH_LONG).show()
-                        return
-                    }
-
-                    val jsonResponse = response.body()
-                    if (jsonResponse != null) {
-                        usuario.access_token = jsonResponse.access_token
-                        Log.i("Token Gerado", "onResponse: " + jsonResponse.toString())
-                        Toast.makeText(this@MainActivity, "Token Gerado", Toast.LENGTH_LONG).show()
-
-//                        var intent: Intent = Intent(this@MainActivity, TelaCadastro:: class.java)
-//                        intent.putExtra("usuario", usuario)
-//                        startActivity(intent)
+//        if(txtEmail.text.toString().isNullOrEmpty() || txtSenha.text.toString().isNullOrEmpty()) {
+//            Toast.makeText(this@MainActivity,
+//                "Dados Não informados.", Toast.LENGTH_LONG).show()
+//
+//            return;
+//        }
+//
+//        usuario = Usuario(
+//            access_token = "",
+//            email = txtEmail.text.toString(),
+//            senha = txtSenha.text.toString()
+//        )
+//
+//        val retrofitCli: RetrofitClient = RetrofitClient()
+//        retrofitCli.servicoUsuario.getToken(usuario)
+//            .enqueue(object : Callback<Usuario> {
+//
+//                override fun onResponse(call: Call<Usuario>, response: Response<Usuario>) {
+//                    if (!response.isSuccessful) {
+//                        Log.e("Usuario ou senha Incorretos", "response: " + response)
+//                        Toast.makeText(this@MainActivity, "Usuario ou senha Incorretos", Toast.LENGTH_LONG).show()
 //                        return
-//                        val extras = intent.extras
-//                        if (extras != null) {
-//                            usuario = getIntent().getSerializableExtra("usuario") as Usuario
-//                            Log.i("testad", usuario.toString())
-//                        }
-                    }
-                }
-
-                override fun onFailure(call: Call<Usuario>, t: Throwable) {
-                    Log.e("Falha ao criar token", "onFailure: " + t)
-                    Toast.makeText(this@MainActivity, "Falha ao criar token", Toast.LENGTH_LONG).show()
-                    return
-                }
-            })
+//                    }
+//
+//                    val jsonResponse = response.body()
+//                    if (jsonResponse != null) {
+//                        usuario.access_token = jsonResponse.access_token
+//                        Log.i("Token Gerado", "onResponse: " + jsonResponse.toString())
+//                        Toast.makeText(this@MainActivity, "Token Gerado", Toast.LENGTH_LONG).show()
+//
+////                        var intent: Intent = Intent(this@MainActivity, TelaCadastro:: class.java)
+////                        intent.putExtra("usuario", usuario)
+////                        startActivity(intent)
+////                        return
+////                        val extras = intent.extras
+////                        if (extras != null) {
+////                            usuario = getIntent().getSerializableExtra("usuario") as Usuario
+////                            Log.i("testad", usuario.toString())
+////                        }
+//                    }
+//                }
+//
+//                override fun onFailure(call: Call<Usuario>, t: Throwable) {
+//                    Log.e("Falha ao criar token", "onFailure: " + t)
+//                    Toast.makeText(this@MainActivity, "Falha ao criar token", Toast.LENGTH_LONG).show()
+//                    return
+//                }
+//            })
 
 
 
