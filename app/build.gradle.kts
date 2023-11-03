af1cbdedccb76cff0f80fa31d99a872aefda2de1
LuisaoxD <105399037+LuisaoxD@users.noreply.github.com>
2023-11-03 19:32:16 -0300

    Inclusão biblioteca Glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,10 @@ dependencies {
     implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("androidx.media3:media3-common:1.1.1")
+    implementation ("androidx.media3:media3-common:1.1.1")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
     testImplementation("junit:junit:4.13.2")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
