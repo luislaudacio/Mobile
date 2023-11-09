d500f7acced21fda21ad6d006501010a91801405
LuisaoxD <105399037+LuisaoxD@users.noreply.github.com>
2023-11-08 23:21:41 -0300

    commit depois do desastre

--- app/src/main/java/com/example/projetointegrador/MinhaConta.kt
+++ app/src/main/java/com/example/projetointegrador/MinhaConta.kt
@@ -2,6 +2,7 @@ package com.example.projetointegrador
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
